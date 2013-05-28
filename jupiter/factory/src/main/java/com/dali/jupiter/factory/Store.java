117c6d347223dc5a094867a77fc936ae80f1fe50
ZIADI Mohamed ali <ma.ziadi@alphalink.fr>
2013-05-28 23:57:57 +0200

    Tests in the Storehouse

--- jupiter/factory/src/main/java/com/dali/jupiter/factory/Store.java
+++ jupiter/factory/src/main/java/com/dali/jupiter/factory/Store.java
@@ -4,23 +4,32 @@ import java.util.Arrays;
 
 public class Store {
 
-  private String[][] availableMaterials;
+  private String[][] availableMaterials={{"iron", "100"}, {"plastic", "100"}, {"liquid", "100"}, {"cartoon", "100"} };
 
   private int[] quantities;
 
   public int getMaterialAvailability(String Material) {
-    return 5;
+    for (int i=0; i<availableMaterials.length;i++) {
+      if (Material.equals(availableMaterials[i][0])) {
+        return Integer.parseInt(availableMaterials[i][1]);
+      } else {
+        return -1;
+      }
+    }
+    return 0;
   }
 
   public String[][] getAllStatistics () {
-    return new String[][] {{"aze", "rty"}, {"uio", "pqs"}};
+    return availableMaterials;
   }
 
   public static void main(String[] args) {
     System.out.println( "--- Classe de Test ---" );
     Store S = new Store();
-    System.out.println(S.getMaterialAvailability("iron"));
     System.out.println(Arrays.toString(S.getAllStatistics()[0]));
-    System.out.println(S.getAllStatistics()[0][0]);
+    for (int i=0; i<S.availableMaterials.length; i++) {
+      System.out.println(S.getAllStatistics()[i][0]);
+    }
+    System.out.println("Availability = " + S.getMaterialAvailability("iron"));
   }
 }
