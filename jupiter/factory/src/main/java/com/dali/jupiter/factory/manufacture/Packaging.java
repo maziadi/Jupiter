dfffdd5bde600368f3189518b65b6aadf4ff8d48
ZIADI Mohamed ali <ma.ziadi@alphalink.fr>
2013-05-28 09:31:17 +0200

    Implementing Production, Validation and Packaging java classes : beginings

---
+++ jupiter/factory/src/main/java/com/dali/jupiter/factory/manufacture/Packaging.java
@@ -0,0 +1,44 @@
+package com.dali.jupiter.factory;
+
+public class Packaging extends  FactoryImpl {
+
+  private String model, color, dimensions;
+
+  private int reference;
+
+  public String getModel () {
+    return model;
+  }
+
+  public void setModel (String model) {
+    this.model = model;
+  }
+
+  public String getColor () {
+    return color;
+  }
+
+  public void setColor (String color) {
+    this.color = color;
+  }
+
+  public String getDimensions () {
+    return dimensions;
+  }
+
+  public void setDimensions (String dimensions) {
+    this.dimensions = dimensions;
+  }
+
+  public int getReference () {
+    return reference;
+  }
+
+  public void setReference (int reference) {
+    this.reference = reference;
+  }
+
+  public boolean designProduct() {
+    return true;
+  }
+}
